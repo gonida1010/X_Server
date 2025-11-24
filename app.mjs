6d91af78484ce7a23627272f6d8729b7e687a2c3
박영곤 <pak101044@gmail.com>
2025-11-24 15:23:23 +0900

    MySQL 적용 완료

--- app.mjs
+++ app.mjs
@@ -2,7 +2,7 @@ import express from "express";
 import postsRouter from "./router/posts.mjs";
 import authRouter from "./router/auth.mjs";
 import { config } from "./config.mjs";
-import { db } from "./db/database.mjs";
+// import { db } from "./db/database.mjs";
 
 // 미들웨어 순서가 맞아야됨
 const app = express();
@@ -16,5 +16,5 @@ app.use((req, res, next) => {
   res.sendStatus(404);
 });
 
-db.getConnection().then((connection) => console.log(connection));
+// db.getConnection().then((connection) => console.log(connection));
 app.listen(config.host.port);
