fd7124a7796a65d41a3a24950ead63363b5f9958
박영곤 <pak101044@gmail.com>
2025-11-25 16:10:03 +0900

    MongoDB 적용 완료

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@ import express from "express";
 import postsRouter from "./router/posts.mjs";
 import authRouter from "./router/auth.mjs";
 import { config } from "./config.mjs";
+import { connectDB } from "./db/database.mjs";
 
 // 미들웨어 순서가 맞아야됨
 const app = express();
@@ -15,4 +16,8 @@ app.use((req, res, next) => {
   res.sendStatus(404);
 });
 
-app.listen(config.host.port);
+connectDB()
+  .then(() => {
+    app.listen(config.host.port);
+  })
+  .catch(console.error);
--- controller/auth.mjs
+++ controller/auth.mjs
@@ -11,7 +11,7 @@ async function createJwtToken(id) {
 }
 
 export async function signup(req, res, next) {
-  const { userid, password, name, email } = req.body;
+  const { userid, password, name, email, url } = req.body;
 
   //회원 중복 체크하기
   const found = await authRepository.findByUserid(userid);
@@ -19,7 +19,13 @@ export async function signup(req, res, next) {
     return res.status(409).json({ message: `${userid}이 이미 있습니다.` });
   }
   const hashed = bcrypt.hashSync(password, config.bcrypt.saltRounds);
-  const user = await authRepository.createUser(userid, hashed, name, email);
+  const user = await authRepository.createUser({
+    userid,
+    password: hashed,
+    name,
+    email,
+    url,
+  });
 
   //   const user = await authRepository.createUser(userid, password, name, email);
   const token = await createJwtToken(user.id);
@@ -44,10 +50,9 @@ export async function login(req, res, next) {
 }
 
 export async function me(req, res, next) {
-  //   const user = await authRepository.findByUserid(req.id);
-  //   if (!user) {
-  //     return res.status(404).json({ message: "일치하는 사용자가 없음" });
-  //   }
-  //   res.status(200).json({ token: req.token, userid: user.userid });
-  res.status(200).json({ message: "성공했어!" });
+  const user = await authRepository.findById(req.id);
+  if (!user) {
+    return res.status(404).json({ message: "일치하는 사용자가 없음" });
+  }
+  res.status(200).json({ token: req.token, userid: user.userid });
 }
